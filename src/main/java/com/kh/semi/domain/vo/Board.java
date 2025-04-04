58245b1d9f4f1facf32bbd0aca45beb9d5c59816
eru2 <steve020205@gmail.com>
2025-04-04 14:30:23 +0900

    ?

--- src/main/java/com/kh/semi/domain/vo/Board.java
+++ src/main/java/com/kh/semi/domain/vo/Board.java
@@ -1,9 +1,6 @@
 package com.kh.semi.domain.vo;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> fedc78d (보드 제작중)
 import lombok.*;
 
 import java.sql.Date;
@@ -13,10 +10,7 @@ import java.sql.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-<<<<<<< HEAD
 @EqualsAndHashCode
-=======
->>>>>>> fedc78d (보드 제작중)
 public class Board {
     private int boardNo;
     private String title;
@@ -29,7 +23,3 @@ public class Board {
     private int userNo;
     private int classNo;
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> fedc78d (보드 제작중)

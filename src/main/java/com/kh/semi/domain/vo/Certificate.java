6f50adb0f7f81fa6981ab6cddf4522f7cd84fe58
eru2 <steve020205@gmail.com>
2025-04-04 10:12:12 +0900

    ?

---
+++ src/main/java/com/kh/semi/domain/vo/Certificate.java
@@ -0,0 +1,21 @@
+package com.kh.semi.domain.vo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Certificate {
+    private int certNo;
+    private String unitName;
+    private String evalDate;
+    private int mainScore;
+    private int finalScore;
+    private int certLevel;
+    private String result;
+    private String filePath;
+    private int userNo;
+}

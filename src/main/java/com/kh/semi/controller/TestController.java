58a239b59fef78b8663bccc0354499e0cab43216
DESKTOP-0BRM7E4\user1 <aadsf1789@naver.com>
2025-04-09 20:51:48 +0900

    경로 중복 문제 해결 (머지 중복 문제)

--- src/main/java/com/kh/semi/controller/TestController.java
+++ src/main/java/com/kh/semi/controller/TestController.java
@@ -29,7 +29,4 @@ public class TestController {
 
     @GetMapping("createCourse.fo")
     public String createClass() {return "admin/adminCreateClass";}
-
-    @GetMapping("adminCourse.li")
-    public String adminCourse() {return "teacher/AdminCourseList";}
 }

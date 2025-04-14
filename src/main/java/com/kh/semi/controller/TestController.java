c2a417d595cbec77f410acf20f790c6cc263d031
jeoung <jmg9152@hs.ac.kr>
2025-04-14 10:09:59 +0900

    테스트 컨트롤러 중복 매핑 수정

--- src/main/java/com/kh/semi/controller/TestController.java
+++ src/main/java/com/kh/semi/controller/TestController.java
@@ -14,24 +14,6 @@ public class TestController {
     public String attendanceManage() {return "student/studentAttendancePageView";}
 
 
-
-
-
-    @GetMapping("infoList.li")
-    public String infoList() {return "teacher/teacherStudentInfoListPage";}
-
-    @GetMapping("vacation.li")
-    public String vacation() {return "teacher/teacherVacationList";}
-    
-
-    @GetMapping("attendance.li")
-    public ModelAndView attendanceList(@RequestParam("classNo") int classNo, ModelAndView mv) {
-        // classNo는 여기서 필요하면 mv에 담거나, 서비스 호출 시 사용할 수 있음
-        mv.addObject("classNo", classNo); // 뷰에서도 ${classNo}로 사용할 수 있게 전달
-        mv.setViewName("teacher/teacherAttendancePage"); // 기존 페이지 그대로 유지
-        return mv;
-    }
-
     @GetMapping("enrollForm.me")
     public String enrollForm() {return "login/memberEnrollPage";}
 
